514f8271eb16831a2e1e0214173355e0d3110bfb
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-10-09 08:06:01 +0545

    using SessionProvider and Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
-import { AppProps } from "next/app";
+import React from "react";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import { Provider as SessionProvider } from "next-auth/client";
+
+import theme from "@utils/theme";
+
+import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <SessionProvider session={pageProps.session}>
+            <ChakraProvider theme={theme}>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+                    />
+                </Head>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </SessionProvider>
     );
 }
 
