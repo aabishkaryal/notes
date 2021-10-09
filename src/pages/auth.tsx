1a276c11cd696d52a4dc92daabe488c672dff503
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-10-09 19:52:03 +0545

    controlled auth UI

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
     Center,
@@ -14,8 +14,8 @@ import {
     VStack,
     InputLeftElement,
     Button,
+    FormControl,
 } from "@chakra-ui/react";
-import { FaRegUser } from "react-icons/fa";
 import { MdOutlineEmail, MdLogin } from "react-icons/md";
 import { FiUserPlus } from "react-icons/fi";
 
@@ -23,6 +23,7 @@ import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/client";
 
 import { PasswordInput } from "@components/passwordInput";
+import { validateEmail } from "@app/utils";
 
 export default function Login() {
     return (
@@ -45,10 +46,31 @@ export default function Login() {
     );
 }
 
+type AuthError = {
+    email?: string;
+    password?: string;
+};
+
 function SignUpPanel() {
+    const [email, updateEmail] = useState("");
+    const [password, updatePassword] = useState("");
+    const [confirmPassword, updateConfirmPassword] = useState("");
+    const [error, updateError] = useState<AuthError>({});
+
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        console.log("Sign Up");
+        console.log("Sign Up", { email, password, confirmPassword });
+        if (!email) {
+            updateError({ email: "Please enter an email." });
+        } else if (!password) {
+            updateError({ password: "Please enter a password." });
+        } else if (!validateEmail(email)) {
+            updateError({ email: "Please enter a valid email." });
+        } else if (password !== confirmPassword) {
+            updateError({ password: "Passwords do not match." });
+        } else {
+            updateError({});
+        }
     };
 
     return (
@@ -66,24 +88,38 @@ function SignUpPanel() {
                 spacing={{ base: 4 }}
                 onSubmit={handleSubmit}
             >
-                <InputGroup>
-                    <InputLeftElement pointerEvents="none" color="gray.500">
-                        <FaRegUser />
-                    </InputLeftElement>
-                    <Input variant="filled" placeholder="Full Name" />
-                </InputGroup>
-                <InputGroup>
-                    <InputLeftElement pointerEvents="none" color="gray.500">
-                        <MdOutlineEmail />
-                    </InputLeftElement>
-                    <Input
-                        variant="filled"
-                        autoComplete="username"
-                        placeholder="Email Address"
-                    />
-                </InputGroup>
-                <PasswordInput placeholder="Password" />
-                <PasswordInput placeholder="Confirm Password" />
+                <FormControl>
+                    <InputGroup>
+                        <InputLeftElement pointerEvents="none" color="gray.500">
+                            <MdOutlineEmail />
+                        </InputLeftElement>
+                        <Input
+                            variant="filled"
+                            autoComplete="username"
+                            placeholder="Email Address"
+                            value={email}
+                            onChange={(e) => updateEmail(e.target.value)}
+                            isInvalid={!!error.email}
+                        />
+                    </InputGroup>
+                    {error.email && (
+                        <Text color="red" fontSize="xs">
+                            {error.email}
+                        </Text>
+                    )}
+                </FormControl>
+                <PasswordInput
+                    placeholder="Password"
+                    value={password}
+                    onChange={(value) => updatePassword(value)}
+                    error={error.password}
+                />
+                <PasswordInput
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(value) => updateConfirmPassword(value)}
+                    error={error.password}
+                />
                 <Button type="submit" leftIcon={<FiUserPlus />}>
                     Sign Up
                 </Button>
@@ -93,9 +129,22 @@ function SignUpPanel() {
 }
 
 function LoginPanel() {
+    const [email, updateEmail] = useState("");
+    const [password, updatePassword] = useState("");
+    const [error, updateError] = useState<AuthError>({});
+
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        console.log("Log In");
+        console.log("Log In", { email, password });
+        if (!email) {
+            updateError({ email: "Please enter an email." });
+        } else if (!password) {
+            updateError({ password: "Please enter a password." });
+        } else if (!validateEmail(email)) {
+            updateError({ email: "Please enter a valid email." });
+        } else {
+            updateError({});
+        }
     };
 
     return (
@@ -113,17 +162,32 @@ function LoginPanel() {
                 spacing={{ base: 4 }}
                 onSubmit={handleSubmit}
             >
-                <InputGroup>
-                    <InputLeftElement pointerEvents="none" color="gray.500">
-                        <MdOutlineEmail />
-                    </InputLeftElement>
-                    <Input
-                        variant="filled"
-                        autoComplete="username"
-                        placeholder="Email Address"
-                    />
-                </InputGroup>
-                <PasswordInput placeholder="Password" />
+                <FormControl>
+                    <InputGroup>
+                        <InputLeftElement pointerEvents="none" color="gray.500">
+                            <MdOutlineEmail />
+                        </InputLeftElement>
+                        <Input
+                            variant="filled"
+                            autoComplete="username"
+                            placeholder="Email Address"
+                            value={email}
+                            onChange={(e) => updateEmail(e.target.value)}
+                            isInvalid={!!error.email}
+                        />
+                    </InputGroup>
+                    {error.email && (
+                        <Text color="red" fontSize="xs">
+                            {error.email}
+                        </Text>
+                    )}
+                </FormControl>
+                <PasswordInput
+                    placeholder="Password"
+                    value={password}
+                    onChange={(value) => updatePassword(value)}
+                    error={error.password}
+                />
                 <Button type="submit" rightIcon={<MdLogin />}>
                     Log In
                 </Button>
