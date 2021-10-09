dd2ad579eb4590af19696384bec1842df0ab2482
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-10-09 15:59:58 +0545

    renamed login to auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export const getServerSideProps: GetServerSideProps<{}> = async (ctx) => {
     if (!(session && session.user))
         return {
             redirect: {
-                destination: "/login",
+                destination: "/auth",
                 permanent: false,
             },
         };
