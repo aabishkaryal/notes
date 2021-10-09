ca137314fd4112d2aac9561f974213b0cb072e25
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-10-09 23:27:32 +0545

    added password validation function

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -3,3 +3,8 @@ export function validateEmail(email: string) {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email.toLowerCase());
 }
+
+export function validatePassword(password: string) {
+    const re = /^(?=(.*[a-zA-Z]){1,})(?=(.*[0-9]){2,}).{8,}$/;
+    return re.test(password);
+}
