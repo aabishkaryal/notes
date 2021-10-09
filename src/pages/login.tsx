80aead2fdb8ee34ec24edc9d7091c8018204f3b8
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-10-09 13:55:42 +0545

    auth tabs made

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,58 @@
-import { Box } from "@chakra-ui/react";
+import React from "react";
+
+import {
+    Box,
+    Flex,
+    Heading,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+} from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/client";
 
-export default function Login() {
-    return <Box>Login</Box>;
+export default function Login({}) {
+    return (
+        <Flex
+            justifyContent="center"
+            alignItems="center"
+            width="100%"
+            height="100vh"
+        >
+            <Tabs boxShadow="md" padding={{ base: 2 }} isLazy>
+                <TabList>
+                    <Tab>Sign Up</Tab>
+                    <Tab>Log In</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <SignUpPanel />
+                    </TabPanel>
+                    <TabPanel>
+                        <LoginPanel />
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </Flex>
+    );
+}
+
+function LoginPanel() {
+    return (
+        <Flex justifyContent="center">
+            <Heading size="lg">Log In</Heading>
+        </Flex>
+    );
+}
+
+function SignUpPanel() {
+    return (
+        <Flex justifyContent="center">
+            <Heading size="lg">Sign Up</Heading>
+        </Flex>
+    );
 }
 
 export const getServerSideProps: GetServerSideProps<{}> = async (ctx) => {
