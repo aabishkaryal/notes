8ef7a826d87d657cc172fe56798d5812e85119a8
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-10-10 16:09:03 +0545

    removed hello api

--- src/pages/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function Hello(req: VercelRequest, res: VercelResponse) {
-    return res.status(200).send("Hello, World!");
-}
