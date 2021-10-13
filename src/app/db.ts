87ec6c9d3cc18aa9d7888061fa094cfae63cfdf4
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-10-13 15:14:00 +0545

    - refactored fetchNotes into fetchCategories and fetchNotes - implemented fetchCategories - dummy fetchNotes

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,6 +1,7 @@
 import Base from "deta/dist/types/base";
 import Drive from "deta/dist/types/drive";
 import { CompositeType } from "deta/dist/types/types/basic";
+import { Category } from "@app/types";
 
 export async function fetchAll<T>(
     db: Base,
@@ -15,6 +16,11 @@ export async function fetchAll<T>(
     return items as any as T[];
 }
 
-export async function fetchNotes(db: Base, drive: Drive, categories: string[]) {
+export async function fetchNotes(drive: Drive, categories: Category[]) {
     return {};
+}
+
+export async function fetchCategories(db: Base, categoryIDs: string[]) {
+    const categories = categoryIDs.map(async (c) => await db.get(c));
+    return categories as any as Category[];
 }
