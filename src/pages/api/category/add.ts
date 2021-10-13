3d738748c20f52d6bb86b1545b1d9fcb4daa12ba
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-10-13 16:33:06 +0545

    return newCategory info for add Category api

--- src/pages/api/category/add.ts
+++ src/pages/api/category/add.ts
@@ -40,7 +40,10 @@ export default async function AddCategory(
         await categoryDB.put(newCategory, categoryUUID);
         return res
             .status(200)
-            .json({ message: `Category ${name} created successfully.` });
+            .json({
+                message: `Category ${name} created successfully.`,
+                category: newCategory,
+            });
     } catch (error) {
         return res.status(500).json({ error: "Failed to add category." });
     }
